677dc373fb40295d2802be08f1170e0223754b45
HotDrify <hotdrify@gmail.com>
2026-04-23 11:35:51 +0300

    new reradme and module FAKER

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# modules
-The Fly Telegram modules repository.
+<div align="center">
+    <img src="https://raw.githubusercontent.com/fly-telegram/userbot/master/assets/logo.gif">
+    <h1>🕊️ fly telegram</h1>
+    <p>It's modules for Fly-Telegram userbot</p>
+</div>
+
+<h2>📥 Installation</h2>
+<p>.dlm https://link-to-module</p>
+
+<table>
+  <tr>
+    <th>Module</th>
+    <th>Link</th>
+  </tr>
+  <tr>
+    <td>Faker</td>
+    <td>https://github.com/fly-telegram/modules/tree/main/community/faker/</td>
+  </tr>
+</table>
---
+++ community/faker/src/main.py
@@ -0,0 +1,36 @@
+#     __ __     __  ___      _ ___
+#    / // /__  / /_/ _ \____(_) _/_ __
+#   / _  / _ \/ __/ // / __/ / _/ // /
+#  /_//_/\___/\__/____/_/ /_/_/ \_, /
+#                              /___/
+#    Licensed under the GNU AGPLv3.
+
+import faker
+import random
+
+
+def fake_cmd(self):
+    fake = faker.Faker()
+
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+
+    age = random.randint(10, 100)
+
+    country = fake.country()
+    address = fake.address()
+    zip_code = fake.zipcode()
+
+    email = fake.email()
+    number = fake.phone_number()
+
+    await self.message.edit(
+        "<b>✨ generated information</b>\n"
+        f"<b>name</b>: <code>{last_name} {first_name}</b>\n"
+        f"<b>age</b>: <code>{age}</b>\n"
+        f"<b>country</b>: <code>{country}</b>\n"
+        f"<b>address</b>: <code>{address}</b>\n"
+        f"<b>ZIP-code</b>: <code>{zip_code}</b>\n"
+        f"<b>email</b>: <code>{email}</b>\n"
+        f"<b>number</b>: <code>{number}</b>\n"
+    )
